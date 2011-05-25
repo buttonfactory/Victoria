7bdebb99742ad8a5649b9d148b8124c04a743337
Jacqueline <jacqueline@thebuttonfactory.nl>
2011-05-25 23:13:01 +0200

    initial meuk

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Victoria::Application.config.secret_token = 'b8fc97d6f6f7d670d12d7d490a03df7a97b3d9f2f9957c7bdf8571a912a54066d4ec35b643e83fb002ea2412bdc0896313b53e48ab42a65e23da3acce7d64e3b'
